9ea33ae2170809f5af553a937bcf2501224bc7e5
Gregory <smartis2816@gmail.com>
2024-01-18 22:35:21 +0300

    project completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,21 @@
-// @todo: Темплейт карточки
-console.log('test');
 
+const template = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+const createCard = function (name, link, deleteCard) {
+    const cardElement = template.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    
+    return cardElement;
+}
+
+const deleteCard = evt => evt.target.closest('.card').remove();
+
+initialCards.forEach(item => placesList.append(createCard(item.name, item.link, deleteCard)));
+
+// @todo: Темплейт карточки
 
 // @todo: DOM узлы
 
