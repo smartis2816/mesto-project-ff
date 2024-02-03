c400d994d59c0302a02296b61c4bdd3097469c0d
Gregory <smartis2816@gmail.com>
2024-02-03 16:06:42 +0300

    project_6_in_progress

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './pages/index.css';
+import { initialCards } from './scripts/cards';
+
+const template = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+const profileEditButton = document.querySelector('.profile__edit-button');
+
+// profileEditButton.addEventListener('click', () => {
+
+// }
+
+const createCard = function (name, link, deleteCard) {
+    const cardElement = template.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    
+    return cardElement;
+}
+
+const deleteCard = evt => evt.target.closest('.card').remove();
+
+initialCards.forEach(item => placesList.append(createCard(item.name, item.link, deleteCard)));
+
+// @todo: Темплейт карточки
+
+// @todo: DOM узлы
+
+// @todo: Функция создания карточки
+
+// @todo: Функция удаления карточки
+
+// @todo: Вывести карточки на страницу
